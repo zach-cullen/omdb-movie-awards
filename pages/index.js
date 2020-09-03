ab8bdad73421e808ca7ba04cac2af549fb2cf60f
Zach Cullen <zacullen@gmail.com>
2020-09-03 15:22:09 -0500

    create input field controlled by state that triggers handler on change

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+  const [searchTerms, setSearchTerms] = useState('')
+
+  const handleSearchChange = event => {
+    setSearchTerms(event.target.value)
+  }
+
   return (
     <main>
-      <h1>OMDB</h1>
+      <h1>The Shoppies</h1>
+      <h2>Movie awards for entrepreneurs</h2>
+      <div>
+        Search for a Movie:
+        <input placeholder='Search' onChange={handleSearchChange} value={searchTerms} />
+      </div>
+      <div>
+        Nominees:
+      </div>
     </main>
   )
 }
