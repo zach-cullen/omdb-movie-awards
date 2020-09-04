c88e59b55fcf7f95488d50de48286c9a2cbd5b17
Zach Cullen <zacullen@gmail.com>
2020-09-04 09:55:50 -0500

    use dark theme in theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../styles/GlobalStyles'
+import { lightTheme, darkTheme } from '../themes/themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
