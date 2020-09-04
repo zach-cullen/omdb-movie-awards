fe131920794e2b98c67f197cebd166799a92c597
Zach Cullen <zacullen@gmail.com>
2020-09-04 14:40:15 -0500

    add grid layout formatting with using styled components

--- components/SearchResultCard.js
+++ components/SearchResultCard.js
@@ -1,8 +1,35 @@
+import styled from 'styled-components'
+
+const CardGrid = styled.div`
+  wdith: 100%;
+  display: grid;
+  grid-template-columns: 60px 1fr;
+  grid-gap: ${props => props.theme.spacing.base};
+  box-sizing: border-box;
+  padding: ${props => props.theme.spacing.baseTight};
+  background-color: ${props => props.theme.colors.bg.secondary};
+  border-radius: ${props => props.theme.spacing.tight};
+  margin-bottom: ${props => props.theme.spacing.base};
+`
+
+const Poster = styled.div`
+  height: 90px;
+  width: 60px;
+  background-color: ${props => props.theme.colors.bg.primary};
+`
+
+const MovieInfo = styled.div`
+
+`
+
 const SearchResultCard = ({ movie }) => {
   return(
-    <div>
-      {movie.Title + ' (' + movie.Year + ')'}
-    </div>
+    <CardGrid>
+      <Poster />
+      <MovieInfo>
+        {movie.Title + ' (' + movie.Year + ')'}
+      </MovieInfo>
+    </CardGrid>
   )
 }
 
