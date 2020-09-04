e1585fb4845c0cc5d836667981d88f75a5d4246c
Zach Cullen <zacullen@gmail.com>
2020-09-04 10:38:29 -0500

    place page content within custom container for centering and padding content

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
+import CustomContainer from '../components/CustomContainer'
 import Search from '../components/Search'
 
 export default function Home() {
   return (
     <main>
-      <h1>The Shoppies</h1>
-      <h2>Movie awards for entrepreneurs</h2>
-      <div>
-        Nominees:
-      </div>
-      <Search />
+      <CustomContainer maxW='1200px' pX='30px'>
+        <h1>The Shoppies</h1>
+        <h2>Movie awards for entrepreneurs</h2>
+        <div>
+          Nominees:
+        </div>
+        <Search />
+      </CustomContainer>
     </main>
   )
 }
