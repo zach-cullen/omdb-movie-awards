486df41a00cd33314238e51405568e6fe0e8afcb
Zach Cullen <zacullen@gmail.com>
2020-09-04 13:19:26 -0500

    use display component instead of bare heading tags

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import CustomContainer from '../components/CustomContainer'
 import Search from '../components/Search'
+import Display from '../components/typographic/Display'
 
 export default function Home() {
   return (
     <main>
       <CustomContainer maxW='1200px' pX='30px'>
-        <h1>The Shoppies</h1>
-        <h2>Movie awards for entrepreneurs</h2>
+        <Display size='x-large'>The Shoppies</Display>
+        <Display size='large'>Movie awards for entrepreneurs</Display>
         <div>
           Nominees:
         </div>
