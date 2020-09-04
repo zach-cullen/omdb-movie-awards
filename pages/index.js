1f80da419b9ff59feb52ee858b1f475c65066e96
Zach Cullen <zacullen@gmail.com>
2020-09-03 21:19:25 -0500

    make search request to api on form submit and save results

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,28 @@ import { useState } from 'react'
 
 export default function Home() {
   const [searchTerms, setSearchTerms] = useState('')
+  const [searchResults, setSearchResults] = useState([])
 
   const handleSearchChange = event => {
     setSearchTerms(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    getSearchResults()
+  }
+
+  const getSearchResults = async () => {
+    const requestUrl = process.env.OMDB_API_URL_BASE + process.env.OMDB_API_KEY + '&s=' + searchTerms
+    const results = await (await fetch(requestUrl)).json()
+    // check Response boolean string provided by api returns 'True' if movie found
+    if (JSON.parse(results.Response.toLowerCase())) {
+      saveSearchResults(results)
+    } 
+  }
+
+  const saveSearchResults = results => {
+    setSearchResults(results)
   }
 
   return (
@@ -14,7 +33,10 @@ export default function Home() {
       <h2>Movie awards for entrepreneurs</h2>
       <div>
         Search for a Movie:
-        <input placeholder='Search' onChange={handleSearchChange} value={searchTerms} />
+        <form onSubmit={handleSubmit}>
+          <input placeholder='Search' onChange={handleSearchChange} value={searchTerms} />
+          <input type='submit' value='Submit' />
+        </form>
       </div>
       <div>
         Nominees:
