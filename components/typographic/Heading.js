1342b876622665641202f7d39399b323c859d4e1
Zach Cullen <zacullen@gmail.com>
2020-09-04 13:30:30 -0500

    format tabs

--- components/typographic/Heading.js
+++ components/typographic/Heading.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 const Heading = styled.h5`
-font-size: 16px;
-line-height: 24px;
-font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 500;
 
   @media (max-width: ${props => props.theme.breakpoints.mobile}) {
     font-size: 17px;
