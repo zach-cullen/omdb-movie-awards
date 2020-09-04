33366ca68790cbb9d56800c728606f8c4a4814bc
Zach Cullen <zacullen@gmail.com>
2020-09-04 09:54:37 -0500

    create light and dark theme objects

---
+++ themes/themes.js
@@ -0,0 +1,21 @@
+export const lightTheme = {
+  bg: {
+    primary: '#fff',
+    secondary: '#dedede'
+  },
+  text: {
+    primary: '#262626',
+    secondary: '#404040'
+  }
+}
+
+export const darkTheme = {
+  bg: {
+    primary: '#262626',
+    secondary: '#404040'
+  },
+  text: {
+    primary: '#e6e6e6',
+    secondary: '#dedede'
+  }
+}
