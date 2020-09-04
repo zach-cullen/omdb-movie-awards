c35f5aaec1e29267d39f62474f30cf498b9b0612
Zach Cullen <zacullen@gmail.com>
2020-09-04 14:20:51 -0500

    add spacing values to theme

--- themes/themes.js
+++ themes/themes.js
@@ -2,9 +2,21 @@ const breakpoints = {
   mobile: '720px'
 }
 
+const spacing = {
+  extraTight: '4px',
+  tight: '8px',
+  baseTight: '12px',
+  base: '16px',
+  loose: '20px',
+  extraLoose: '24px',
+}
+
 export const lightTheme = {
   breakpoints: {
     ...breakpoints
+  },
+  spacing: {
+    ...spacing
   },
   colors: {
     bg: {
@@ -21,6 +33,9 @@ export const lightTheme = {
 export const darkTheme = {
   breakpoints: {
     ...breakpoints
+  },  
+  spacing: {
+    ...spacing
   },
   colors: {
     bg: {
