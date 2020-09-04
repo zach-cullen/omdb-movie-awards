6ec4d42d7686a2dc2368e274848b2c574652c40f
Zach Cullen <zacullen@gmail.com>
2020-09-04 14:22:24 -0500

    use spacing values from theme

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 const StyledInput = styled.input`
   display: block;
   box-sizing: border-box;
-  padding: 8px;
+  padding: ${props => props.theme.spacing.tight};
   width: 100%;
-  border-radius: 4px;
+  margin: ${props => props.theme.spacing.baseTight} 0;
+  border-radius: ${props => props.theme.spacing.extraTight};
   background-color: ${props => props.theme.colors.bg.secondary};
 `
 
