a3971e7a14222a955ff43b69b9ee3658b2e651b8
Zach Cullen <zacullen@gmail.com>
2020-09-04 14:08:20 -0500

    make styled component and add styles

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,6 +1,17 @@
+import styled from 'styled-components'
+
+const StyledInput = styled.input`
+  display: block;
+  box-sizing: border-box;
+  padding: 8px;
+  width: 100%;
+  border-radius: 4px;
+  background-color: ${props => props.theme.colors.bg.secondary};
+`
+
 const SearchForm = ({ handleSearchChange, searchTerms }) => {
   return(
-    <input 
+    <StyledInput
       placeholder='Enter a movie title' 
       onChange={handleSearchChange} 
       value={searchTerms} 
