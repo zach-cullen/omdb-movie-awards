2e89d2c8828cd7d09b95762404e230253bfe684d
Zach Cullen <zacullen@gmail.com>
2020-09-05 14:32:34 -0500

    shrink cards for mobile to fit more in row

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -3,8 +3,12 @@ import SearchResultCard from './SearchResultCard'
 
 const ResultsGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: ${props => props.theme.spacing.loose};
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: ${props => props.theme.spacing.baseTight};
+
+  @media (max-width: 720px) {
+    grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
+  }
 `
 
 const SearchResults = ({ movies }) => (
