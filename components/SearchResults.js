f60cf70d9cb610b8ad264dafaf9792ee2582537f
Zach Cullen <zacullen@gmail.com>
2020-09-05 14:16:13 -0500

    use auto-fill grid to responsively create rows of cards

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,13 +1,20 @@
+import styled from 'styled-components'
 import SearchResultCard from './SearchResultCard'
 
+const ResultsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: ${props => props.theme.spacing.loose};
+`
+
 const SearchResults = ({ movies }) => (
-  <div>
+  <ResultsGrid>
     { movies.map((movie, i) => {
       return (
         <SearchResultCard key={i} movie={movie}/>
       )
     })}
-  </div>
+  </ResultsGrid>
 )
 
 export default SearchResults
