005a9b142ef3010e8ef7f4cde1fef0d33ccdb548
Zach Cullen <zacullen@gmail.com>
2020-09-06 16:19:26 -0500

    render nominees in nominee card grid

--- components/Nominees.js
+++ components/Nominees.js
@@ -1,8 +1,32 @@
-const Nominees = () => {
+import styled from 'styled-components'
+import NomineeCard from './NomineeCard'
+
+const NomineesGrid = styled.div`
+  min-height: 100px;
+  width: 100%;
+  margin: ${props => props.theme.spacing.baseTight} 0;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: ${props => props.theme.spacing.tight};
+
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`
+
+const Nominees = ({ nominees }) => {
+
   return(
-    <div>
-      Nominees
-    </div>
+    <>
+      My Nominees:
+      <NomineesGrid>
+        {nominees.map(movie => {
+          return(
+            <NomineeCard movie={movie} />
+          )
+        })}
+      </NomineesGrid>
+    </>
   )
 }
 
