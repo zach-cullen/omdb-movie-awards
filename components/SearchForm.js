2eb931ac1022ba40a44d315541a4f04d53c6460a
Zach Cullen <zacullen@gmail.com>
2020-09-07 22:28:00 -0500

    restyle with search icon and reset button that calls prop function clearing search bar

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,22 +1,68 @@
+import { useRef } from 'react'
 import styled from 'styled-components'
+import { SearchMinor, ResetMinor } from '@shopify/polaris-icons'
+
+const SearchBar = styled.div`
+  height: 36px;
+  /* max-width: 480px; */
+  display: grid;
+  overflow: hidden;
+  grid-template-columns: 36px 1fr 36px;
+  border-radius: ${props => props.theme.spacing.tight};
+  background-color: ${props => props.theme.colors.bg.secondary};
+  margin: ${props => props.theme.spacing.base} 0;
+`
+
+const IconWrapper = styled.div`
+  width: 20px;
+  height: 20px;
+  padding: 8px;
+  fill: ${props => props.theme.colors.text.primary};
+
+  .button-icon {
+    cursor: pointer;
+    fill: ${props => props.theme.colors.text.secondary};
+    transition: fill 0.2s;
+
+    &:hover {
+      fill: ${props => props.theme.colors.text.primary};
+    }
+  }
+`
 
 const StyledInput = styled.input`
   display: block;
-  box-sizing: border-box;
-  padding: ${props => props.theme.spacing.tight};
+  height: 36px;
   width: 100%;
-  margin: ${props => props.theme.spacing.baseTight} 0;
-  border-radius: ${props => props.theme.spacing.extraTight};
-  background-color: ${props => props.theme.colors.bg.secondary};
+  margin-left: ${props => props.theme.spacing.tight};
+  font-size: 16px;
+  background-color: inherit;
 `
 
-const SearchForm = ({ handleSearchChange, searchTerms }) => {
+const SearchForm = ({ handleSearchChange, resetSearchTerms, searchTerms }) => {
+  const searchFieldRef = useRef(null)
+
+  const handleResetClick = () => {
+    resetSearchTerms()
+  }
+
   return(
-    <StyledInput
-      placeholder='Enter a movie title' 
-      onChange={handleSearchChange} 
-      value={searchTerms} 
-    />
+    <SearchBar>
+      <IconWrapper>
+        <SearchMinor />
+      </IconWrapper>
+      <StyledInput
+        placeholder='Enter a movie title' 
+        onChange={handleSearchChange} 
+        value={searchTerms} 
+      />
+      <IconWrapper>
+        <ResetMinor 
+          className='button-icon'
+          onClick={handleResetClick}
+        />
+      </IconWrapper>
+    </SearchBar>
   )
 }
 
