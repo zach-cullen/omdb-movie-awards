7179c112c4f18f30d87e224349b5f7b51e7e1799
Zach Cullen <zacullen@gmail.com>
2020-09-08 06:43:49 -0500

    animate hide and show of welcome message based on whether there are any nominees

--- components/WelcomeMessage.js
+++ components/WelcomeMessage.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import ShoppiesLogoSVG from './ShoppiesLogoSVG'
 import Display from './typographic/Display'
@@ -5,45 +6,66 @@ import Body from './typographic/Body'
 import SubHeading from './typographic/SubHeading'
 
 const WelcomeContainer = styled.div`
-  margin: ${props => props.theme.spacing.extraLoose} auto;
+  height: ${props => props.isLoaded ? '100%' : '0px'};
+  margin: ${props => props.isLoaded ? props.theme.spacing.extraLoose : 0} auto;
   box-sizing: border-box;
   color: ${props => props.theme.colors.text.primary};
 `
 
 const LogoWrapper = styled.div`
-  width: 80px;
+  width: ${props => props.isLoaded ? '80px' : '40px'};
   margin: 16px auto;
   fill: ${props => props.theme.colors.text.primary};
+  opacity: ${props => props.isLoaded ? 1 : 0};
+  transition: width ${props => props.isLoaded ? '0.5s' : '0.1s'}, 
+    opacity ${props => props.isLoaded ? '0.5s' : '0.1s'} ease-in-out;
 `
 
 const WelcomeText = styled.div`
   width: 100%;
-  height: 100%;
   text-align: center;
+  opacity: ${props => props.isLoaded ? 1 : 0};
+  transition: opacity ${props => props.isLoaded ? '0.5s' : '0.1s'} ease-in-out;
 
   .title-text {
     font-size: 24px;
     line-height: 30px;
     font-weight: 900;
     letter-spacing: 0.25em;
-    margin: 8px 0;
+    margin: ${props => props.theme.spacing.baseTight} 0;
   }
 `
 
-const WelcomeMessage = () => {
+const WelcomeMessage = ({ nomineeCount }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    if (nomineeCount === 0) {
+      setIsLoaded(true)
+    } else {
+      setIsLoaded(false)
+    }
+  }, [nomineeCount])
+
   return(
-    <WelcomeContainer>
-      <LogoWrapper>
+    <WelcomeContainer
+      isLoaded={isLoaded}
+    >
+      <LogoWrapper
+        isLoaded={isLoaded}
+      >
         <ShoppiesLogoSVG />
       </LogoWrapper>
-      <WelcomeText>
+      <WelcomeText
+        isLoaded={isLoaded}
+      >
         <div className='title-text'>
           THE SHOPPIES
         </div>
         <SubHeading>
           Movie Awards for Entrepreneurs
         </SubHeading>
-        <br />
+        <br/>
         <Display size='small'>
           Nominate up to 5 of your favorite movies.
         </Display>
